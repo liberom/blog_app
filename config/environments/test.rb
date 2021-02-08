410d4129e0d4275e301c469eca2040cee9548c01
Vinicius Libero <libero.moreira@gmail.com>
2021-02-08 16:12:14 -0300

    created controller and routes and index view for articles

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -47,5 +47,5 @@ Rails.application.configure do
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
-  config.autoload_paths << "#{config.root}/spec/lib"
+  # config.autoload_paths << "#{config.root}/spec/lib"
 end
